71999419f4c01af2c7f015fae0a3fdc4fb8d1c94
Brian Cox <brianlcox2009@gmail.com>
2020-07-23 12:03:17 -0700

    starting question js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+//Login and Home Page
+
 //hides login page after they press login
 $(".btnLogin").click(function(){
     $("#loginPage").addClass("hide");
@@ -24,3 +26,54 @@ $(".btnLogin").click(function(){
   $(document).ready(function(){
     $('.carousel').carousel();
   });
+
+
+  //Questions page//
+$(document).ready(function(){
+
+  var correctCount= 0;
+  var userGuess ="";
+
+function displayQuestion(){
+  var questionCard = `<div class="row">
+  <div class="col s12">
+      <h4 class="catagoryQuestion center" id="catagoryTrivia">Sports</h4>
+  </div>
+</div>
+<!--Question display-->
+<div class="row">
+  <div class="col s12 m6">
+      <div class="card">
+          <div class="card-content">
+              <span class="card-title">Question: <span
+                      class="questionNumber">1/10:</span>
+                  <p class="cardQuestions">I am a very simple card. I am good at containing small bits of
+                      information.
+                      I am convenient because I require little markup to use effectively.</p>
+          </div>
+      </div>
+      <div class="row answerOne">
+          <div class="col s12">
+              <a class="waves-effect waves-light buttonA1 btn-large">Answer Text, Test Test</a>
+          </div>
+      </div>
+      <div class="row answerOne">
+          <div class="col s12">
+              <a class="waves-effect waves-light btn-large btnAnswer">Answer Text, Test Test Test</a>
+          </div>
+      </div>
+      <div class="row answerOne">
+          <div class="col s12">
+              <a class="waves-effect waves-light btn-large btnAnswer">Answer Text, Test Test Test</a>
+          </div>
+      </div>
+      <div class="row answerOne">
+          <div class="col s12">
+              <a class="waves-effect waves-light btn-large btnAnswer">Answer Text, Test Test Test</a>
+          </div>
+      </div>
+  </div>
+</div>`
+$(`.container`).append(questionCard)
+}
+})
